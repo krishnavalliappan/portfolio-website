3bbe90930e58bb53b203d3a4d1455e3861338c88
krishnavalliappan <krishnavalliappan02@gmail.com>
2024-08-24 16:33:47 -0400

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
       </head>
       <body className={`${jetbrain_mono.variable} ${inter.variable} font-mono antialiased`}>
         <ThemeProvider
